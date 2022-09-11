3294e78260e3a61d6abd08f297dd6f3b926ffd13
huseyinbalicak <huseyin.balicak@condictum.be>
2022-09-11 14:55:28 +0300

    Added api to return To Do list of a selected category.

--- src/main/java/com/appcent/todoist/controller/CategoryController.java
+++ src/main/java/com/appcent/todoist/controller/CategoryController.java
@@ -1,10 +1,12 @@
 package com.appcent.todoist.controller;
 
 import com.appcent.todoist.dto.CategoryResponseDto;
+import com.appcent.todoist.dto.TodoResponseDto;
 import com.appcent.todoist.dto.save.CategorySaveRequestDto;
 import com.appcent.todoist.dto.update.CategoryUpdateRequestDto;
 import com.appcent.todoist.response.RestResponse;
 import com.appcent.todoist.service.CategoryService;
+import com.appcent.todoist.service.TodoService;
 import lombok.RequiredArgsConstructor;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.DeleteMapping;
@@ -25,11 +27,20 @@ import java.util.List;
 public class CategoryController {
 
     private final CategoryService categoryService;
+    private final TodoService todoService;
 
     @GetMapping
     public ResponseEntity findAll(){
         List<CategoryResponseDto> categoryResponseDtoList = categoryService.findAll();
         return ResponseEntity.ok(RestResponse.of(categoryResponseDtoList));
+    }
+
+    @GetMapping("/todos/{id}")
+    public ResponseEntity getAllTodoByCategoriesId(@PathVariable  Long id) {
+
+        List<TodoResponseDto> todoResponseDtoList = todoService.findByCategory(id);
+        return ResponseEntity.ok(RestResponse.of(todoResponseDtoList));
+
     }
 
     @GetMapping("/{id}")
